d9bb5d428305ecc61a726e04a23b11fb1c2f1d55
Simon Corless <simon@sico.co.uk>
2013-07-28 23:02:51 +0100

    Initial commit of existing code

---
+++ lang/English/sico_avatars.php
@@ -0,0 +1,6 @@
+<?php
+// Language definitions used in sico_avatars
+$lang_sico_avatar = array(
+	'Avatar Warning' => 'You must not upload MGM Copyrighted images for use as avatars, this means no stills from the movies, no posters and no custom compilations containing any copyrighted images. - If you do so your avatar will be removed without notice.',
+);
+?>
