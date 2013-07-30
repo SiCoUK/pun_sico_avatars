a2b9c750c734e79ebec99764340e7637f0bdf863
Simon Corless <simon@sico.co.uk>
2013-07-30 08:44:05 +0100

    Added Gravatar descriptions

--- lang/English/sico_avatars.php
+++ lang/English/sico_avatars.php
@@ -1,7 +1,9 @@
 <?php
 // Language definitions used in sico_avatars
 $lang_sico_avatar = array(
-	'Avatar Warning' => 'You must not upload MGM Copyrighted images for use as avatars, this means no stills from the movies, no posters and no custom compilations containing any copyrighted images. - If you do so your avatar will be removed without notice.',
-        'Gravatar' => 'Your Gravatar',
+    'Avatar Warning' => 'You must not upload MGM Copyrighted images for use as avatars, this means no stills from the movies, no posters and no custom compilations containing any copyrighted images. - If you do so your avatar will be removed without notice.',
+    'Gravatar Description' => 'You can use a service called Gravatar which allows you to setup web wide avatars for your email address (your registered forum address), this service allows you to upload any size image and the correct size will be shown on the forum. If you do not have a forum avatar then your Gravatar will be used, Gravatar use can also be disabled if you wish.',
+    'Gravatar' => 'Your Gravatar',
+    'Setup Gravatar' => 'Setup your Gravatar'
 );
 ?>
