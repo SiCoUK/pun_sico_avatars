dd46b51ff0efd6860c0c35558235aeee3e512cf5
Simon Corless <simon@sico.co.uk>
2013-07-31 08:36:28 +0100

    Added admin functions and install/uninstall

--- functions.php
+++ functions.php
@@ -13,16 +13,45 @@ function select_gravatar()
 {
     global $forum_config, $lang_sico_avatar, $forum_user, $forum_page;
     
-    // Show the gravatar
-    ?>
-    <div class="ct-set set<?php echo ++$forum_page['item_count'] ?>">
-        <div class="ct-box">
-            <h3 class="hn ct-legend"><?php echo $lang_sico_avatar['Gravatar'] ?></h3>
-            <p class="avatar-demo"><span><?php echo get_gravatar($forum_user['email']); ?></span></p>
-            <p><a href="http://www.gravatar.com/"><?php echo $lang_sico_avatar['Setup Gravatar'] ?></a></p>
+    print_r($forum_config['o_sico_avatars_gravatar']);
+    
+    // Show the gravatar if allowed
+    if (!empty($forum_config['o_sico_avatars_gravatar'])) {
+        ?>
+        <div class="ct-set set<?php echo ++$forum_page['item_count'] ?>">
+            <div class="ct-box">
+                <h3 class="hn ct-legend"><?php echo $lang_sico_avatar['Gravatar'] ?></h3>
+                <p class="avatar-demo"><span><?php echo get_gravatar($forum_user['email']); ?></span></p>
+                <p><a href="http://www.gravatar.com/"><?php echo $lang_sico_avatar['Setup Gravatar'] ?></a></p>
+            </div>
         </div>
+        <?
+    }
+}
+
+function sico_avatar_admin_options() {
+    
+    global $forum_config, $lang_sico_avatar, $forum_page;
+    
+    $forum_page['group_count'] = $forum_page['item_count'] = 0;
+
+    ?>
+    <div class="content-head">
+        <h2 class="hn"><span><?php echo $lang_sico_avatar['Avatar admin head'] ?></span></h2>
     </div>
-    <?
+    <fieldset class="frm-group group<?php echo ++$forum_page['group_count'] ?>">
+        <legend class="group-legend"><span><?php echo $lang_sico_avatar['Gravatar admin legend'] ?></span></legend>
+        <div class="sf-set set<?php echo ++$forum_page['item_count'] ?>">
+            <div class="sf-box checkbox">
+                <span class="fld-input"><input type="checkbox" id="fld<?php echo ++$forum_page['fld_count'] ?>" name="form[sico_avatars_gravatar]" value="1"<?php if ($forum_config['o_sico_avatars_gravatar'] == '1') echo ' checked="checked"' ?> /></span>
+                <label for="fld<?php echo $forum_page['fld_count'] ?>"><span><?php echo $lang_sico_avatar['Gravatar admin legend'] ?></span><?php echo $lang_sico_avatar['Gravatar info'] ?></label>
+            </div>
+        </div>
+    </fieldset>
+    <?php
+
+    // Reset fieldset counter
+    $forum_page['set_count'] = 0;
 }
 
 /**
